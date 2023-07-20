1cdd7155d21089ee04920245f79714920311d684
pokachip <pokachip@outlook.com>
2023-07-20 18:03:58 +0900

    inital upload

---
+++ SSH_MAC_CHECK/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace SSH_MAC_CHECK
+{
+    partial class F1CS43752_MAC_CHECK
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F1CS43752_MAC_CHECK));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.text_MAC = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(58, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(318, 439);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Linux PC\r\nPOWER OFF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_PowerOff);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.richTextBox1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(8, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(436, 208);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // text_MAC
+            // 
+            this.text_MAC.Font = new System.Drawing.Font("Verdana", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_MAC.ForeColor = System.Drawing.Color.Black;
+            this.text_MAC.Location = new System.Drawing.Point(5, 330);
+            this.text_MAC.Name = "text_MAC";
+            this.text_MAC.Size = new System.Drawing.Size(444, 54);
+            this.text_MAC.TabIndex = 4;
+            this.text_MAC.Text = "MAC ADDR";
+            this.text_MAC.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(300, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 54);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "MAC Check";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_MACcheck);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(58, 419);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 27);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(58, 459);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(196, 27);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 424);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Start";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 464);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "End";
+            // 
+            // F1CS43752_MAC_CHECK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(453, 498);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.text_MAC);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "F1CS43752_MAC_CHECK";
+            this.Text = "F1CS43752_MAC_CHECK";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label text_MAC;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SSH_MAC_CHECK/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci.SshNet;
+using Renci.SshNet.Common;
+using System.Threading;
+
+
+/// <summary>
+/// https://stackoverflow.com/questions/41555597/how-to-run-commands-by-sudo-and-enter-password-by-ssh-net-c-sharp
+/// </summary>
+
+namespace SSH_MAC_CHECK
+{
+    public partial class F1CS43752_MAC_CHECK : Form
+    {
+        public F1CS43752_MAC_CHECK()
+        {
+            InitializeComponent();
+            ////////////////////////////////////////////////////////////////////////////////// read IP in txt file
+
+            
+            string input = "";
+            string[] lines = System.IO.File.ReadAllLines(@"C:\43752IP.txt", Encoding.Default);
+            foreach (string show in lines)
+            {
+                if (input == "")
+                {
+                    input = show;
+                }
+                else
+                {
+                    input = input + "\r\n" + show;
+                }
+            }
+            
+            //TextBox에 넣기
+            textBox1.Text = input;
+            //textBox1.Text = "192.168.10.130";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button_PowerOff(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "")
+                MessageBox.Show("Host Ip를 입력해주세요");
+                
+            else
+            {
+                SshClient cSSH = new SshClient(textBox1.Text, 22, "f1media", "f1media");
+                cSSH.Connect();
+                //cSSH.KeepAliveInterval = TimeSpan.FromSeconds(5);
+                //cSSH.ConnectionInfo.Timeout = TimeSpan.FromSeconds(10);
+                if (cSSH.IsConnected)
+                {
+
+                    //SshCommand cmd_PWROFF = 
+                    cSSH.RunCommand("echo -e 'f1media\n' |  sudo -S shutdown -P now");
+                    //SshCommand cmd_PWROFF = cSSH.RunCommand("./Desktop/F1CS43752/work/SSH_Shutdown.sh");
+                    //cSSH.Disconnect();
+                    //cSSH.Dispose();
+                    //richTextBox1.AppendText("SSH Disconnected\n");
+                    Application.ExitThread();
+                    Application.Exit();
+                }
+                else
+                {
+                    Application.ExitThread();
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_MACcheck(object sender, EventArgs e)
+        {
+            string Macaddr = "00:90:4c:12:d0:01";
+            string Initmac = "00:90:4c:12:d0:01\n";
+            if (textBox1.Text == "") 
+            {
+                MessageBox.Show("Host Ip를 입력해주세요");              
+            }
+            else
+            {
+                SshClient cSSH = new SshClient(textBox1.Text, 22, "f1media", "f1media");
+                cSSH.Connect();
+
+                if (cSSH.IsConnected)
+                {
+                    richTextBox1.Clear();
+                    text_MAC.Text = "";
+
+                    //Scan PCIe slot
+                    richTextBox1.AppendText("Scan PCIe Slot\n");
+                    SshCommand cmd_rescan = cSSH.RunCommand("echo -e 'f1media\n' | sudo -S ./Desktop/F1CS43752/work/rscan.sh");
+                    richTextBox1.SelectionColor = System.Drawing.Color.Blue;
+                    richTextBox1.AppendText(cmd_rescan.Result + "\n");
+
+                    // Load Drive
+                    richTextBox1.AppendText("Load driver\n");
+                    SshCommand cmd_load = cSSH.RunCommand("./Desktop/F1CS43752/work/SSHload.sh");
+                    richTextBox1.AppendText(cmd_load.Result + "\n");
+
+                    SshCommand tes2t = cSSH.RunCommand("ifconfig | grep wlan0");
+                    richTextBox1.AppendText(tes2t.Result);
+
+                    // Display MAC address
+                    SshCommand test = cSSH.RunCommand("ifconfig wlan0 | grep ether | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'");
+                    Macaddr = test.Result;
+                    if (Macaddr.Equals(Initmac))
+                    {
+                        text_MAC.ForeColor = Color.Red;
+                        text_MAC.Text = "Failed";
+                    } else{
+                        text_MAC.ForeColor = Color.Blue;
+                        text_MAC.Text = Macaddr; //test.Result;
+                    }
+                    
+                    
+                    // remove pcie
+                    SshCommand cmd_remove = cSSH.RunCommand("echo -e 'f1media\n' | sudo -S ./Desktop/F1CS43752/work/rmove.sh");
+                    richTextBox1.AppendText(cmd_remove.Result + "\n");
+                    richTextBox1.AppendText("PCIe removed\n");
+                    richTextBox1.AppendText("\n");
+                    // Disconnect SSH
+                    cSSH.Disconnect();
+                    cSSH.Dispose();
+                    richTextBox1.AppendText("SSH Disconnected\n");
+                  
+                }
+            }
+        }
+    }
+}
